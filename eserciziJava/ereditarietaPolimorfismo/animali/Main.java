02930e6e48f8f0d9ea3514b73b3f524f09bf4fb4
Manila <78729862+manilaexe@users.noreply.github.com>
2026-06-25 21:52:42 +0200

    Add Main class to demonstrate animal polymorphism

---
+++ eserciziJava/ereditarietaPolimorfismo/animali/Main.java
@@ -0,0 +1,9 @@
+public class Main1 {
+    public static void main(String[] args) {
+        Animale a1 = new Cane("Luna");
+        Animale a2 = new Gatto("Stella");
+
+        a1.verso();
+        a2.verso();
+    }
+}
