ca964e492adf7bf32fd3342f2c5e1ca5fd543de9
Manila <78729862+manilaexe@users.noreply.github.com>
2026-06-25 21:56:53 +0200

    Create Dipendente.java

---
+++ eserciziJava/ereditarietaPolimorfismo/dipendenti/Dipendente.java
@@ -0,0 +1,11 @@
+class Dipendente {
+    protected String nome;
+
+    public Dipendente(String nome) {
+        this.nome = nome;
+    }
+
+    public double stipendio() {
+        return 0;
+    }
+}
