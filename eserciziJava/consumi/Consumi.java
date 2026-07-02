7fa532111c77a24f9a7d44728b32e95e60a20c1a
Manila <78729862+manilaexe@users.noreply.github.com>
2026-07-02 08:05:42 +0200

    Update and rename calcolaConsumi.java to Consumi.java

--- eserciziJava/consumi/Consumi.java
+++ eserciziJava/consumi/Consumi.java
@@ -3,17 +3,26 @@ package consumi;
 public class Consumi {
 
     public static void main(String[] args) {
+
         Auto miaAuto = new Auto();
+
         System.out.println("Esercizio 1");
         miaAuto.calcolaConsumi();
+
         System.out.println("Esercizio 3");
-        /*
+
         try {
-            miaAuto.rifornisci("Metano",20);
-        } catch(CarburanteErratoException e) {
+
+            miaAuto.rifornisci("Metano", 20);
+
+        } catch (CarburanteErratoException e) {
+
             System.out.println(e.getMessage());
+
         }
-         */
+
         System.out.println("Esercizio 4");
+
     }
+
 }
