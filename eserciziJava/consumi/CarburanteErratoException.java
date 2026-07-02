ef5994c61536742c40839ece79fc63b3a30b7eb3
Manila <78729862+manilaexe@users.noreply.github.com>
2026-07-02 08:03:18 +0200

    Create CarburanteErratoException.java

---
+++ eserciziJava/consumi/CarburanteErratoException.java
@@ -0,0 +1,9 @@
+package consumi;
+
+public class CarburanteErratoException extends Exception {
+
+    public CarburanteErratoException() {
+        super("È stato inserito il carburante errato!!!");
+    }
+
+}
