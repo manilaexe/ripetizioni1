b3899277bbd7aa79db41a322cdf64504d80efbb9
Manila <78729862+manilaexe@users.noreply.github.com>
2026-07-02 08:05:18 +0200

    Add Auto class for fuel consumption calculations
    The class includes methods for calculating fuel consumption and refueling, with error handling for invalid inputs.

---
+++ eserciziJava/consumi/Auto.java
@@ -0,0 +1,83 @@
+package consumi;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Auto {
+
+    private double km_l = 0;
+    public static final int CAPIENZA_MAX = 80;
+    private int livello_carburante;
+
+    private String tipoCarburante;
+
+    public Auto() {
+        tipoCarburante = "Benzina";
+        livello_carburante = 0;
+    }
+
+    public String getTipoCarburante() {
+        return tipoCarburante;
+    }
+
+    public void setTipoCarburante(String tipoCarburante) {
+        this.tipoCarburante = tipoCarburante;
+    }
+
+    public void calcolaConsumi() {
+
+        Scanner tastiera = new Scanner(System.in);
+
+        try {
+
+            int kilometri, litri;
+
+            System.out.print("Inserire i kilometri percorsi: ");
+            kilometri = tastiera.nextInt();
+
+            System.out.print("Inserire i litri di benzina consumati: ");
+            litri = tastiera.nextInt();
+
+            if (litri == 0) {
+                throw new ArithmeticException();
+            }
+
+            km_l = (double) kilometri / litri;
+
+            System.out.println("La tua auto fa " + km_l + " kilometri al litro");
+            System.out.println("... fine dell'esercizio.");
+
+        } catch (ArithmeticException e) {
+
+            System.out.println("Sicuro che la tua auto non consumi carburante?");
+
+        } catch (InputMismatchException e) {
+
+            System.out.println("Devi inserire dei dati numerici validi");
+
+        } catch (Exception e) {
+
+            System.out.println("Errore imprevisto:");
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public void rifornisci(String tipoCarburante, int quantita)
+            throws CarburanteErratoException {
+
+        if (!tipoCarburante.equalsIgnoreCase(this.tipoCarburante)) {
+            throw new CarburanteErratoException();
+        }
+
+        livello_carburante += quantita;
+
+        if (livello_carburante > CAPIENZA_MAX) {
+            livello_carburante = CAPIENZA_MAX;
+        }
+
+        System.out.println("Rifornimento effettuato.");
+    }
+
+}
