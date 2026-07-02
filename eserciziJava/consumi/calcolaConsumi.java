72f2594a837ea708bff73814ff26c7f206e40e9b
Manila <78729862+manilaexe@users.noreply.github.com>
2026-07-02 08:05:04 +0200

    Update calcolaConsumi.java

--- eserciziJava/consumi/calcolaConsumi.java
+++ eserciziJava/consumi/calcolaConsumi.java
@@ -1,36 +1,19 @@
-public void calcolaConsumi() {
+package consumi;
 
-    Scanner tastiera = new Scanner(System.in);
+public class Consumi {
 
-    try {
-
-        int kilometri, litri;
-
-        System.out.print("Inserire i kilometri percorsi: ");
-        kilometri = tastiera.nextInt();
-
-        System.out.print("Inserire i litri di benzina consumati: ");
-        litri = tastiera.nextInt();
-
-        if (litri == 0) {
-            throw new ArithmeticException();
+    public static void main(String[] args) {
+        Auto miaAuto = new Auto();
+        System.out.println("Esercizio 1");
+        miaAuto.calcolaConsumi();
+        System.out.println("Esercizio 3");
+        /*
+        try {
+            miaAuto.rifornisci("Metano",20);
+        } catch(CarburanteErratoException e) {
+            System.out.println(e.getMessage());
         }
-
-        km_l = (double) kilometri / litri;
-
-        System.out.println("La tua auto fa " + km_l + " kilometri al litro");
-        System.out.println("... fine dell'esercizio.");
-
+         */
+        System.out.println("Esercizio 4");
     }
-    catch (ArithmeticException e) {
-        System.out.println("Sicuro che la tua auto non consumi carburante?");
-    }
-    catch (java.util.InputMismatchException e) {
-        System.out.println("Devi inserire dei dati numerici validi");
-    }
-    catch (Exception e) {
-        System.out.println("Errore imprevisto:");
-        e.printStackTrace();
-    }
-
 }
