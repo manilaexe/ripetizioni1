5adb90501f784e3ecbedd147dc3c0d1378e3d676
Manila <78729862+manilaexe@users.noreply.github.com>
2026-07-07 08:40:36 +0200

    Create Nodo.java

---
+++ eserciziJava/linkedList/Nodo.java
@@ -0,0 +1,32 @@
+public class Nodo {
+
+    private int dato;
+    private Nodo next;
+
+
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.next = null;
+    }
+
+
+    public int getDato() {
+        return dato;
+    }
+
+
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+
+
+    public Nodo getNext() {
+        return next;
+    }
+
+
+    public void setNext(Nodo next) {
+        this.next = next;
+    }
+
+}
