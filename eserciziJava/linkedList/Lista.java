f6fca406701fb5dca4fa7260959f4d03da7fcae0
Manila <78729862+manilaexe@users.noreply.github.com>
2026-07-07 10:42:20 +0200

    Update Lista.java

--- eserciziJava/linkedList/Lista.java
+++ eserciziJava/linkedList/Lista.java
@@ -11,9 +11,7 @@ public class Lista {
     public void inserisciInTesta(int valore) {
 
         Nodo nuovo = new Nodo(valore);
-
         nuovo.setNext(testa);
-
         testa = nuovo;
     }
 
@@ -21,63 +19,36 @@ public class Lista {
     public void inserisciInCoda(int valore) {
 
         Nodo nuovo = new Nodo(valore);
-
-
         if (testa == null) {
-
             testa = nuovo;
-
         } else {
-
             Nodo corrente = testa;
-
-
             while (corrente.getNext() != null) {
-
                 corrente = corrente.getNext();
-
             }
-
-
             corrente.setNext(nuovo);
         }
-
     }
 
     // STAMPA LISTA
-
     public void stampa() {
-
+        
         Nodo corrente = testa;
-
-
         while (corrente != null) {
-
             System.out.println(corrente.getDato());
-
             corrente = corrente.getNext();
-
         }
-
     }
 
     // CONTA ELEMENTI
     public int contaElementi() {
 
         int contatore = 0;
-
         Nodo corrente = testa;
-
-
         while (corrente != null) {
-
             contatore++;
-
             corrente = corrente.getNext();
-
         }
-
-
         return contatore;
     }
 
@@ -85,24 +56,40 @@ public class Lista {
     public boolean cerca(int valore) {
 
         Nodo corrente = testa;
-
-
         while (corrente != null) {
-
-
             if (corrente.getDato() == valore) {
-
                 return true;
-
             }
-
-
             corrente = corrente.getNext();
-
         }
-
-
         return false;
     }
 
+    //inserimento in posizione random
+    public void inserisciInPosizione(int valore, int posizione) {
+
+        Nodo nuovo = new Nodo(valore);
+        
+        // Lista vuota oppure inserimento in testa
+        if (posizione <= 0 || testa == null) {
+            nuovo.setNext(testa);
+            testa = nuovo;
+            return;
+        }
+    
+    
+        Nodo corrente = testa;
+        int indice = 0;   
+        
+        // Arrivo al nodo precedente alla posizione richiesta
+        while (corrente.getNext() != null && indice < posizione - 1) {
+            corrente = corrente.getNext();
+            indice++;  
+        }
+    
+        // Collegamenti
+        nuovo.setNext(corrente.getNext());
+        corrente.setNext(nuovo);
+    }
+    
 }
